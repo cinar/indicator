5df5aba0221522f351014522ad6141add8760ab1
Onur Cinar <onur.cinar@gmail.com>
2021-09-28 01:19:22 +0000

    Moving least square and moving linear regression using least square are added.

--- regression.go
+++ regression.go
@@ -1,5 +1,9 @@
 package indicator
 
+import (
+	"log"
+)
+
 // Least square.
 //
 // y = mx + b
@@ -27,16 +31,72 @@ func LeastSquare(x, y []float64) (float64, float64) {
 	return m, b
 }
 
+// Moving least square over a period.
+//
+// y = mx + b
+// b = y-intercept
+// y = slope
+//
+// m = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX)
+// b = (sumY - m * sumX) / n
+func MovingLeastSquare(period int, x, y []float64) ([]float64, []float64) {
+	checkSameSize(x, y)
+
+	m := make([]float64, len(x))
+	b := make([]float64, len(x))
+
+	var sumX, sumX2, sumY, sumXY float64
+
+	for i := 0; i < len(x); i++ {
+		sumX += x[i]
+		sumX2 += x[i] * x[i]
+		sumY += y[i]
+		sumXY += x[i] * y[i]
+
+		n := float64(i + 1)
+
+		if i >= period {
+			sumX -= x[i-period]
+			sumX2 -= x[i-period] * x[i-period]
+			sumY -= y[i-period]
+			sumXY -= x[i-period] * y[i-period]
+			n = float64(period)
+		}
+
+		m[i] = ((n * sumXY) - (sumX * sumY)) / ((n * sumX2) - (sumX * sumX))
+		b[i] = (sumY - (m[i] * sumX)) / n
+	}
+
+	return m, b
+}
+
 // Linear regression using least square method.
 //
 // y = mx + b
 func LinearRegressionUsingLeastSquare(x, y []float64) []float64 {
 	m, b := LeastSquare(x, y)
 
-	r := make([]float64, len(y))
+	r := make([]float64, len(x))
 
 	for i := 0; i < len(r); i++ {
 		r[i] = (m * x[i]) + b
+	}
+
+	return r
+}
+
+// Moving linear regression using least square.
+//
+// y = mx + b
+func MovingLinearRegressionUsingLeastSquare(period int, x, y []float64) []float64 {
+	m, b := MovingLeastSquare(period, x, y)
+
+	r := make([]float64, len(x))
+	log.Println(m)
+	log.Println(b)
+
+	for i := 0; i < len(r); i++ {
+		r[i] = (m[i] * x[i]) + b[i]
 	}
 
 	return r
--- regression_test.go
+++ regression_test.go
@@ -25,6 +25,29 @@ func TestLeastSquare(t *testing.T) {
 	}
 }
 
+func TestMovingLeastSquare(t *testing.T) {
+	x := []float64{1, 2, 3, 4, 2, 3, 5, 7, 9}
+	y := []float64{1, 2, 3, 4, 4, 5, 7, 10, 15}
+
+	period := 5
+
+	m, b := MovingLeastSquare(period, x, y)
+
+	expectedM := 1.5183
+	expectedB := 0.3049
+
+	finalM := roundDigits(m[len(m)-1], 4)
+	finalB := roundDigits(b[len(b)-1], 4)
+
+	if finalM != expectedM {
+		t.Fatalf("m %f expected %f", finalM, expectedM)
+	}
+
+	if finalB != expectedB {
+		t.Fatalf("m %f expected %f", finalB, expectedB)
+	}
+}
+
 func TestLinearRegressionUsingLeastSquare(t *testing.T) {
 	x := []float64{0, 2, 5, 7}
 	y := []float64{-1, 5, 12, 20}
