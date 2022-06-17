be38121e04a9c73f376ecff4e08ac720dcb1a9ea
Onur Cinar <onur.cinar@gmail.com>
2022-06-17 21:49:49 +0000

    Renaming the test function.

--- helper_test.go
+++ helper_test.go
@@ -123,7 +123,7 @@ func TestAddBy(t *testing.T) {
 	}
 }
 
-func Testsubtract(t *testing.T) {
+func TestSubtract(t *testing.T) {
 	values1 := []float64{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
 	values2 := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
