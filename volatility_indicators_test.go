1386a4e602aaf20fc340108bba25e35967d5ab17
Onur Cinar <onur.cinar@gmail.com>
2022-06-23 22:42:27 +0000

    Bollinger Bands standard deviation calculation is different from other implementations Fixes #102

--- volatility_indicators_test.go
+++ volatility_indicators_test.go
@@ -11,7 +11,7 @@ import (
 
 func TestStd(t *testing.T) {
 	values := []float64{2, 4, 6, 8, 12, 14, 16, 18, 20}
-	expected := []float64{0, 0.707, 1, 1, 1.581, 1.581, 1, 1, 1}
+	expected := []float64{0, 1, 1, 1, 2, 1 ,1, 1, 1}
 	period := 2
 
 	actual := Std(period, values)
