acce729b5dd3c9cb159673faad7bb17e9b4c4d82
Onur Cinar <onur.cinar@gmail.com>
2022-06-24 01:57:03 +0000

    Bollinger bands test case.

--- volatility_indicators_test.go
+++ volatility_indicators_test.go
@@ -9,9 +9,44 @@ import (
 	"testing"
 )
 
+func TestBollingerBands(t *testing.T) {
+	closing := []float64{
+		2, 4, 6, 8, 12, 14, 16, 18, 20,
+		2, 4, 6, 8, 12, 14, 16, 18, 20,
+		2, 4, 6, 8, 12, 14, 16, 18, 20,
+		2, 4, 6, 8, 12, 14, 16, 18, 20,
+	}
+
+	expectedMiddleBand := []float64{
+		2, 3, 4, 5, 6.4, 7.67, 8.86, 10, 11.11,
+		10.2, 9.64, 9.33, 9.23, 9.43, 9.73, 10.13, 10.59, 11.11,
+		10.63, 10.3, 10.5, 10.7, 11, 11.3, 11.5, 11.7, 11.9,
+		11.1, 10.3, 10.5, 10.7, 11, 11.3, 11.5, 11.7, 11.9,
+	}
+
+	expectedUpperBand := []float64{
+		2, 3, 4, 5, 6.4, 7.67, 8.86, 10, 11.11, 10.2,
+		9.64, 9.33, 9.23, 9.43, 9.73, 10.13, 10.59, 11.11,
+		10.63, 22.78, 22.56, 22.45, 22.56, 22.84, 23.22, 23.72, 24.32,
+		23.9, 22.78, 22.56, 22.45, 22.56, 22.84, 23.22, 23.72, 24.32,
+	}
+
+	expectedLowerBand := []float64{
+		2, 3, 4, 5, 6.4, 7.67, 8.86, 10, 11.11,
+		10.2, 9.64, 9.33, 9.23, 9.43, 9.73, 10.13, 10.59, 11.11,
+		10.63, -2.18, -1.56, -1.05, -0.56, -0.24, -0.22, -0.32, -0.52,
+		-1.7, -2.18, -1.56, -1.05, -0.56, -0.24, -0.22, -0.32, -0.52,
+	}
+
+	actualMiddleBand, actualUpperBand, actualLowerBand := BollingerBands(closing)
+	testEquals(t, roundDigitsAll(actualMiddleBand, 2), expectedMiddleBand)
+	testEquals(t, roundDigitsAll(actualUpperBand, 2), expectedUpperBand)
+	testEquals(t, roundDigitsAll(actualLowerBand, 2), expectedLowerBand)
+}
+
 func TestStd(t *testing.T) {
 	values := []float64{2, 4, 6, 8, 12, 14, 16, 18, 20}
-	expected := []float64{0, 1, 1, 1, 2, 1 ,1, 1, 1}
+	expected := []float64{0, 1, 1, 1, 2, 1, 1, 1, 1}
 	period := 2
 
 	actual := Std(period, values)
