cde33561d057fff5ec2f80333fee2401cc07a8fa
Onur Cinar <onur.cinar@gmail.com>
2024-12-24 06:14:42 +0000

    Mark the remove functions as test helper. functions.

--- helper/remove.go
+++ helper/remove.go
@@ -11,6 +11,8 @@ import (
 
 // Remove removes the file with the given name.
 func Remove(t *testing.T, name string) {
+	t.Helper()
+
 	err := os.Remove(name)
 	if err != nil {
 		t.Errorf("Error removing file: %v", err)
@@ -19,6 +21,8 @@ func Remove(t *testing.T, name string) {
 
 // RemoveAll removes the files with the given path.
 func RemoveAll(t *testing.T, path string) {
+	t.Helper()
+
 	err := os.RemoveAll(path)
 	if err != nil {
 		t.Errorf("Error removing files: %v", err)
