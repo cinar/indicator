c2e2acb2ceac4b54bd5acaf079bb5da02f799521
Onur Cinar <onur.cinar@gmail.com>
2025-01-18 20:28:21 +0000

    Added SQL repository support for assets.

---
+++ helper/database.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2021-2024 Onur Cinar.
+// The source code is provided under GNU AGPLv3 License.
+// https://github.com/cinar/indicator
+
+package helper
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+// CloseDatabaseWithError closes the database after an error.
+func CloseDatabaseWithError(db *sql.DB, err error) error {
+	closeErr := db.Close()
+	if closeErr == nil {
+		return err
+	}
+
+	closeErr = fmt.Errorf("unable to close database: %w", closeErr)
+
+	if err != nil {
+		log.Println(closeErr)
+		return err
+	}
+
+	return closeErr
+}
+
+// CloseDatabaseRows closes the database rows.
+func CloseDatabaseRows(rows *sql.Rows) {
+	err := rows.Close()
+	if err != nil {
+		log.Println(err)
+	}
+}
