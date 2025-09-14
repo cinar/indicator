557e6898bd560fe6ee35e4507d5d1cdd7fcbd1c4
Onur Cinar <onur.cinar@gmail.com>
2025-09-14 15:49:35 +0000

    Future Value (FV) valuation.

---
+++ valuation/fv.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2021-2024 Onur Cinar.
+// The source code is provided under GNU AGPLv3 License.
+// https://github.com/cinar/indicator
+
+package valuation
+
+import "math"
+
+// Fv calculates the Future Value (FV) of a Present Value (PV).
+//
+//  Formula: FV = PV * (1 + rate)^years
+func Fv(pv, rate float64, years int) float64 {
+	return pv * math.Pow((1+rate), float64(years))
+}
