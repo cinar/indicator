aff23daa800e3007ae915f24632c538950fabbc0
Onur Cinar <onur.cinar@gmail.com>
2026-02-28 22:46:07 -0800

    Implement Ichimoku Cloud Strategy #336 (#348)
    Implement the Ichimoku Cloud trading strategy using the existing
    indicator.
    
    Fixed #336
    
    # Change Type
    
    - New strategy: Ichimoku Cloud Strategy
    - Documentation: README.md, strategy/momentum/README.md
    - Tests: Ichimoku Cloud strategy tests with historical data.

--- strategy/momentum/momentum.go
+++ strategy/momentum/momentum.go
@@ -24,6 +24,7 @@ import "github.com/cinar/indicator/v2/strategy"
 func AllStrategies() []strategy.Strategy {
 	return []strategy.Strategy{
 		NewAwesomeOscillatorStrategy(),
+		NewIchimokuCloudStrategy(),
 		NewRsiStrategy(),
 		NewStochasticRsiStrategy(),
 		NewTripleRsiStrategy(),
