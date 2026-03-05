f56f822ab4c3f5b8aebd4d569346617ef5954350
Eonyak Cho <eonyakcho@gmail.com>
2026-03-05 21:11:00 +0000

    Williams R Strategy added. Issue 337

---
+++ strategy/momentum/williams_r_strategy_test.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2021-2026 Onur Cinar.
+// The source code is provided under GNU AGPLv3 License.
+// https://github.com/cinar/indicator
+
+package momentum_test
+
+import (
+	"testing"
+
+	"github.com/cinar/indicator/v2/asset"
+	"github.com/cinar/indicator/v2/helper"
+	"github.com/cinar/indicator/v2/strategy"
+	"github.com/cinar/indicator/v2/strategy/momentum"
+)
+
+func TestWilliamsRStrategy(t *testing.T) {
+	snapshots, err := helper.ReadFromCsvFile[asset.Snapshot]("testdata/brk-b.csv")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	results, err := helper.ReadFromCsvFile[strategy.Result]("testdata/williams_r_strategy.csv")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := helper.Map(results, func(r *strategy.Result) strategy.Action { return r.Action })
+
+	wr := momentum.NewWilliamsRStrategy()
+	actual := wr.Compute(snapshots)
+
+	err = helper.CheckEquals(actual, expected)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestWilliamsRStrategyReport(t *testing.T) {
+	snapshots, err := helper.ReadFromCsvFile[asset.Snapshot]("testdata/brk-b.csv")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wr := momentum.NewWilliamsRStrategy()
+
+	report := wr.Report(snapshots)
+
+	fileName := "williams_r_strategy.html"
+	defer helper.Remove(t, fileName)
+
+	err = report.WriteToFile(fileName)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
